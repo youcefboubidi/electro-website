5d558b1839baf53d2f6a31e0155144fa7e1e7edb
akram <son.gogo@hotmail.com>
2024-03-02 22:27:46 +0100

    optimised  hero section | made a dropdown menu for the navbar :3

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,17 +4,18 @@ import React from 'react'
 function Hero() {
     const paragraph = 'Once upon a time, in the whimsical land of Cagts, there lived a giant cat named Whiskerius the Grand. Whiskerius wasn’t just any ordinary feline; he was the undisputed ruler of all Cagts. His fur shimmered like the golden sands of the desert, and his eyes glowed with the wisdom of a thousand lifetimes.'
     return (
-        <>
-            <div className="grid grid-cols-2 mb-12">
-                <div className="flex flex-col justify-center gap-8 col-span-1">
+        <div className='cursor-default'>
+            <div className="flex justify-between ">
+                <div className="flex flex-col   gap-16 ">
 
-                    <h1 className="text-6xl squada-one-regular">Electro Anti-Social Club</h1>
-                    <p className='text-2xl'>{paragraph}</p>
+                    <h1 className="text-6xl squada-one-regular ">Electro Anti-Social Club</h1>
+                    <p className='text-2xl mr-40' >{paragraph}</p>
                 </div>
-                <img src="/hero.png" alt="" className='my-auto ms-auto' />
-
+               
+                <img src="/hero.png" alt="" className='my-auto ms-auto rounded-3xl' />
+               
             </div>
-        </>
+        </div>
     )
 }
 
--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,36 +1,67 @@
 import Image from 'next/image';
 import React from 'react';
 import Link from 'next/link'; // Add missing import statement
+import { useState } from 'react';
 
-function NavBar() {
-    return (
-        <nav className="flex justify-between text-2xl  py-10 px-16">
-            <div className='flex gap-4 items-center'>
-                <Link href="/"></Link>
-                <Image
+
+    function NavBar() {
+        const [isOpen, setIsOpen] = useState(false);
+
+        return (
+            <nav className="flex justify-between text-2xl py-10 px-16 pb-24 ">
+                <div className='flex gap-4 items-center'>
+                    <Link href="/"></Link>
+                    <Image
                     src='/logo all white 2.png'
                     width={51}
                     height={50}
+                    className='cursor-default '
+                    />
+
+                    <span className=' text-[24px] font-[400] cursor-default '>ELECTRO CLUB</span>
+                </div>
+                {isOpen ? (
+                    <div className=' flex flex-col gap-16 lg:flex-row  '>
+                        <Link href="/" className='text-[24px] font-[400]  transition-all cursor-pointer   hover:font-medium '>
+                            HOME    
+                        </Link>
+                        <Link href="/projects" className='text-[24px] font-[400] transition-all cursor-pointer  hover:font-medium '>
+                            PROJECTS
+                        </Link>
+                        <Link href="/events" className='text-[24px] font-[400]  transition-all cursor-pointer  hover:font-medium '>
+                            EVENTS
+                        </Link>
+                        <Link href="/entertainment" className='text-[24px] font-[400]  transition-all cursor-pointer  hover:font-medium'>
+                            ENTERTAINMENT
+                        </Link>
+                    </div>
+                ) : <div className='  flex-row hidden lg:flex gap-16  '>
+                        <Link href="/" className='text-[24px] font-[400]  transition-all cursor-pointer   hover:font-medium '>
+                            HOME    
+                        </Link>
+                        <Link href="/projects" className='text-[24px] font-[400] transition-all cursor-pointer  hover:font-medium '>
+                            PROJECTS
+                        </Link>
+                        <Link href="/events" className='text-[24px] font-[400]  transition-all cursor-pointer  hover:font-medium '>
+                            EVENTS
+                        </Link>
+                        <Link href="/entertainment" className='text-[24px] font-[400]  transition-all cursor-pointer  hover:font-medium'>
+                            ENTERTAINMENT
+                        </Link>
+                    </div>
+                    }
+                <Image
+                    src='/menu.jpg'
+                    alt='menu'
+                    width={100}
+                    height={100}
+                    className='inline-block cursor-pointer lg:hidden fixed right-20 '
+                    onClick={() => setIsOpen(!isOpen)}
+                    layout='fixed' // Add this line to prevent the image from stretching
                 />
+            </nav >
+        );
+    }
 
-                <span className='font-bold'>ELECTRO CLUB</span>
-            </div>
-            <div className='flex justify-end gap-16'>
-                <Link href="/" className='font-light transition-all duration-500 hover:font-medium '>
-                    HOME
-                </Link>
-                <Link href="/projects" className='font-light transition-all duration-300 hover:font-medium '>
-                    PROJECTS
-                </Link>
-                <Link href="/events" className='font-light transition-all duration-300 hover:font-medium '>
-                    EVENTS
-                </Link>
-                <Link href="/entertainment" className='font-light transition-all duration-300 hover:font-medium'>
-                    ENTERTAINMENT
-                </Link>
-            </div>
-        </nav >
-    );
-}
+    export default NavBar;
 
-export default NavBar;

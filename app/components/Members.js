68bf0d2404d53322d9c0016ebb2c93d0c81649fd
akram <son.gogo@hotmail.com>
2024-03-04 17:08:31 +0100

    color change :p

--- app/components/Members.js
+++ app/components/Members.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Members = () => {
   return (
-    <div className='flex justify-around align-center rounded-lg text-lg p-10 bg-slate-700 cursor-default'>
+    <div className='flex justify-around align-center rounded-lg text-lg p-10 bg-[#2a303b] cursor-default'>
       
          <div className='flex flex-col items-center gap-3'><h3 className='text-4xl font-semibold'>132  </h3><p className='font-extralight ' >Members</p> </div>
          <div className='flex flex-col items-center gap-3'><h3 className='text-4xl font-semibold'>13  </h3><p className='font-extralight '>Projects</p> </div>
